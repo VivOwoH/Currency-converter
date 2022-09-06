b36240ca06e2990478f6ef1b91600acc2e65e1a3
Faye Chen <faye2011@kimo.com>
2022-09-06 13:07:30 +1000

    fix conflicts

--- app/src/main/java/a1/view/CurrencySelect.java
+++ app/src/main/java/a1/view/CurrencySelect.java
@@ -7,14 +7,14 @@ import java.awt.event.*;
 
 import javax.swing.JComboBox;
 
-public class CurrencySelect extends JComboBox<String> {
+public class CurrencySelect extends JComboBox<String> implements ActionListener{
     private final Window window;
     private final Syst system;
 
     private String[] currencyList;
 
     private final int height = 75;
-    private final int width = 75;
+    private final int width = 100;
 
     public CurrencySelect(Window window, int x, int y) {
         super(Syst.getCurrencies());
